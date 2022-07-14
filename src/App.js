21a14671adaa0f8d75a7712e912e65384ef71e77
Vikrant_Mulankar <vikrantmulankar24021998@gmail.com>
2022-07-14 13:50:25 +0530

    post

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Task from './Components/Task';
+
+function App() {
+  return (
+    <div>
+      <Task />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Task.js
@@ -0,0 +1,61 @@
+import React from 'react'
+
+export default function Task() {
+  return (
+    <div>
+        <div className="container">
+            <h1>OUR USP</h1>
+            
+            <div className="box-container">
+                <div className="text-box">
+
+                <div className="box">
+                    <img src="./Pulse Oximeter.svg" alt="" />
+                </div>
+                <p>Patentable Products</p>
+                </div>
+
+                <div className="text-box">
+
+                <div className="box">
+                    <img src="./Pulse Oximeter.svg" alt="" />
+                </div>
+                <p>Novel Technology</p>
+                </div>
+
+                <div className="text-box">
+
+                <div className="box">
+                    <img src="./Pulse Oximeter.svg" alt="" />
+                </div>
+                <p>Focus On Multiple Industries</p>
+                </div>
+
+                <div className="text-box">
+
+                <div className="box">
+                    <img src="./Pulse Oximeter.svg" alt="" />
+                </div>
+                <p>Multiple Industries</p>
+                </div>
+
+                <div className="text-box">
+
+                <div className="box">
+                    <img src="./Pulse Oximeter.svg" alt="" />
+                </div>
+                <p>Commercial & Personal Use</p>
+                </div>
+
+                <div className="text-box">
+
+                <div className="box">
+                    <img src="./Pulse Oximeter.svg" alt="" />
+                </div>
+                <p>Right Pricing</p>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
